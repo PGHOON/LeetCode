6ac3c16524295f78d5e124f14e108248c5e4fc1c
PGHOON <park1578@purdue.edu>
2024-06-05 14:22:47 +0900

    [Feat] solved 80. Remove Duplicates from Sorted Array II

--- Top_Interview_100/80.c
+++ Top_Interview_100/80.c
@@ -3,10 +3,15 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int k = 2;
         int length = nums.size();
+
+        if(length < k){
+            return length;
+        }
+
         for (int i = 2; i < length; i++){
             if(nums[k - 2] != nums[i]){
                 nums[k] = nums[i];
-                k++;
+                k += 1;
             }
         }
         return k;
